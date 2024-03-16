9eec429d846f1a243d7374167b8583e9ac50d123
JinnR1106 <157051418+JinnR1106@users.noreply.github.com>
2024-03-16 22:36:01 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function toCelsius(fahrenheit)
+{
+    let value = (fahrenheit - 32) *5 / 9
+    return value.toFixed(2) + "℃";
+}
+function display(elementId, value)
+{
+    document.getElementById(elementId).innerHTML = "<b>"+value+"</b>"
+}
+function toFahrenheit(celsius)
+{
+    let value = (celsius * 9) / 5 +32
+    return value.toFixed(2) + "°F";
+}
+function toCelsiusProgram(value)
+{
+    alert(toCelsius(value))
+
+}
+function toFahrenheitProgram(value)
+{
+    alert(toFahrenheit(value))
+
+}
